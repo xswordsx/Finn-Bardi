ccfce4d56595685b91f7392c7654ca88e803f42a
Ivan Latunov <xswordsx@gmail.com>
2014-06-02 16:34:51 +0300

    Rework Tile class

--- tile.py
+++ tile.py
@@ -3,12 +3,20 @@ import pygame
 
 class Tile(pygame.sprite.Sprite):
 
-    def __init__(self, x, y, tile_image_path, screen, block_size):
+    def __init__(self, x, y, tile_image_path, surface, block_size):
+
         pygame.sprite.Sprite.__init__(self)
+
         self.__position = (x, y)
+
         self.__image = pygame.image.load(tile_image_path)
         self.__image = pygame.transform.scale(self.__image, block_size)
-        self.__screen = screen
+        self.__image.convert()
+
+        self.__surface = surface
+        
+        self.rect = self.__image.get_rect()
+        self.rect.topleft = self.__position
 
     def update(self):
-        self.__screen.blit(self.__image, self.__position)
+        self.__surface.blit(self.__image, self.__position)
