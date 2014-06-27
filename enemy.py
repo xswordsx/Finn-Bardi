b3c0cc2a0a463f2c0b847351ffe9b4b15b1b9771
Ivan Latunov <xswordsx@gmail.com>
2014-06-28 02:32:15 +0300

    Introduce damage

--- enemy.py
+++ enemy.py
@@ -5,3 +5,15 @@ class Enemy:
         self._stats = stats
         self._img_path = img_path
         self._loot = loot
+        self._current_hp = -1
+
+    def inflict_damage(self):
+        # Returns damage, that it can inflict
+        pass
+
+    def take_damage(self):
+        # Takes some damage
+        pass
+
+    def is_alive(self):
+        return self._current_hp > 0
