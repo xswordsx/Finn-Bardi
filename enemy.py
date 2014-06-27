31d771ae32c841074fda6cce4f31e758d80194b8
Ivan Latunov <xswordsx@gmail.com>
2014-06-28 02:29:00 +0300

    Add lootable items

--- enemy.py
+++ enemy.py
@@ -1,6 +1,7 @@
 class Enemy:
 
-    def __init__(name, stats, img_path):
+    def __init__(name, stats, img_path, loot):
         self._name = name
         self._stats = stats
         self._img_path = img_path
+        self._loot = loot
